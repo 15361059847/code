22c1245c34ddc28674211cdac0d61a38b2f94cf1
15361059847 <525504136@qq.com>
2019-09-06 16:58:09 +0800

    测试配置

--- order/server/src/main/java/com/immoc/config/TestController.java
+++ order/server/src/main/java/com/immoc/config/TestController.java
@@ -1,32 +1,32 @@
-//package com.immoc.config;
-//
-//import org.springframework.amqp.core.AmqpTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Date;
-//
-///**
-// * Created by lenovo on 2019/8/30.
-// */
-//@RestController
-//@RequestMapping("/test")
-//@RefreshScope
-//public class TestController {
-//
-//
-//
-//    @Value("${env}")
-//    private String order;
-//
-//    @GetMapping("/print")
-//    public String print(){
-//        return  order;
-//    }
-//
-//
-//}
+package com.immoc.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by lenovo on 2019/8/30.
+ */
+@RestController
+@RequestMapping("/test")
+@RefreshScope
+public class TestController {
+
+
+
+    @Value("${env}")
+    private String order;
+
+    @GetMapping("/print")
+    public String print(){
+        return  order;
+    }
+
+
+}
