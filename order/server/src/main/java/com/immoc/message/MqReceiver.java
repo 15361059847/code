8845d532f149683029d73484ea72ebdb9d78c0e6
15361059847 <525504136@qq.com>
2019-09-09 11:39:00 +0800

    代码

--- order/server/src/main/java/com/immoc/message/MqReceiver.java
+++ order/server/src/main/java/com/immoc/message/MqReceiver.java
@@ -1,6 +1,9 @@
 package com.immoc.message;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +15,45 @@ import org.springframework.stereotype.Component;
 @Component
 public class MqReceiver {
 
-    @RabbitListener(queues = "myQueue")
+    //1、@RabbitListener(queues = "myQueue")
+    //2、自动创建队列  @RabbitListener(queuesToDeclare = @Queue("myQueue"))
+    //3、自从创建，Exchange和Queue绑定。
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue("myQueue"),
+            exchange = @Exchange("myExchange")
+            )
+    )
     public void process(String message){
         log.info("MqReceiver：{}" + message);
+    }
+
+
+    /**
+     * 数码供应商服务接收消息
+     * @param message
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange("myOrder"),
+            key = "computer",
+            value = @Queue("computerOrder")
+        )
+    )
+    public void computer(String message){
+        log.info("computerOrder MqReceiver：{}" + message);
+    }
+
+
+    /**
+     * 水果供应商服务接收消息
+     * @param message
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange("myOrder"),
+            key = "fruit",
+            value = @Queue("fruitOrder")
+        )
+    )
+    public void fruit(String message){
+        log.info("fruit MqReceiver：{}" + message);
     }
 }
