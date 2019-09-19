0aac5857e0eb8c8348d5ed0ed72fd1e2073a5ac1
15361059847 <525504136@qq.com>
2019-09-19 08:48:41 +0800

    路由

---
+++ api-gateway/src/main/java/com/immoc/ZuulConfig.java
@@ -0,0 +1,19 @@
+//package com.immoc;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * Created by lenovo on 2019/9/18.
+// */
+//@Component
+//public class ZuulConfig {
+//
+//    @ConfigurationProperties("zuul")
+//    @RefreshScope
+//    public ZuulProperties zuulProperties(){
+//        return new ZuulProperties();
+//    }
+//}
